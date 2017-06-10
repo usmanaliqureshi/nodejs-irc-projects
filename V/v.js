7c7e5157d9a207e3588834902104a1e8606e35aa
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-06-10 14:58:33 +0500

    Added basic oper functionality like login, sethost and mode

--- V/v.js
+++ V/v.js
@@ -1,46 +1,74 @@
 var irc = require("irc");
 
+var fs = require("fs");
+
 var vbot = new irc.Client('newyork.nationchat.org', 'V', {
 
     channels: ['#N-Bots'],
 
 });
 
+vbot.addListener('registered', function() {
+
+	fs.readFile('./credentials.json', 'utf-8', function(error, credentials) {
+
+		credentials =  JSON.parse(credentials);
+
+		vbot.send('oper', credentials.login, credentials.password);
+
+		vbot.send('sethost', 'request', 'vhost.service');
+
+		vbot.send('mode', vbot.nick, '+oiwksgxnIWazrh');
+
+	});
+
+});
+
+vbot.addListener('join', function(channel, nick, message) {
+
+	if ( nick === vbot.nick ) {
+
+		vbot.send('opmode', channel, '+o', nick);
+
+	}
+
+});
+
 vbot.addListener('message#', function(from, to, message) {
 
-		vbot.whois(from, function(info) {
+	vbot.whois(from, function(info) {
 
-			if ( (info.nick == 'BOSS') && (info.user == 'BOSS') && (info.host == 'The.BOSS') ) {
+		if ( (info.nick == 'BOSS') && (info.user == 'BOSS') && (info.host == 'The.BOSS') ) {
 
-				if ( (message === '.op') && (to = '#N-Bots') ) {
+			if ( (message === '.op') && (to = '#N-Bots') ) {
 
-					console.log('OPPED ' + from + ' in ' + to + ' - USER VERIFIED');
+				console.log('OPPED ' + from + ' in ' + to + ' - USER VERIFIED');
 
-					vbot.send('MODE', to, '+o', from);
+				vbot.send('MODE', to, '+o', from);
 
-				} else if ( (message === '.deop') && (to = '#N-Bots') ) {
+			} else if ( (message === '.deop') && (to = '#N-Bots') ) {
 
-					console.log('DEOPPED ' + from + ' in ' + to + ' - USER VERIFIED');
+				console.log('DEOPPED ' + from + ' in ' + to + ' - USER VERIFIED');
 
-					vbot.send('MODE', to, '-o', from);
+				vbot.send('MODE', to, '-o', from);
 
-				} else if ( (message === '.voice') && (to = '#N-Bots') ) {
+			} else if ( (message === '.voice') && (to = '#N-Bots') ) {
 
-					console.log('VOICED ' + from + ' in ' + to + ' - USER VERIFIED');
+				console.log('VOICED ' + from + ' in ' + to + ' - USER VERIFIED');
 
-					vbot.send('MODE', to, '+v', from);
+				vbot.send('MODE', to, '+v', from);
 
-				} else if ( (message === '.devoice') && (to = '#N-Bots') ) {
+			} else if ( (message === '.devoice') && (to = '#N-Bots') ) {
 
-					console.log('DEVOICED ' + from + ' in ' + to + ' - USER VERIFIED');
+				console.log('DEVOICED ' + from + ' in ' + to + ' - USER VERIFIED');
 
-					vbot.send('MODE', to, '-v', from);
-
-				}
+				vbot.send('MODE', to, '-v', from);
 
 			}
 
-		});
+		}
+
+	});
 
 });
 
